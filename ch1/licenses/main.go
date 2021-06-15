0cf79e6e414877e6300679d93569246ae54ad782
leelixiangjun <leelixiangjun@didiglobal.com>
2021-06-15 22:07:21 +0800

    add

--- ch1/licenses/main.go
+++ ch1/licenses/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"image/color"
+	"image/gif"
 	"math/rand"
 	"os"
 	"time"
@@ -20,5 +21,14 @@ func main() {
 }
 
 func lissajous(out *os.File) {
+	const (
+		cycles  = 5
+		res     = 0.001 // angular resolution
+		size    = 100   // image canvas covers [-size..+size]
+		nframes = 64    // number of animation frames
+		delay   = 8     // delay between frames in 10ms units
+	)
+	freq := rand.Float64() * 3.0
+	anim := gif.GIF{LoopCount: nframes}
 
 }
